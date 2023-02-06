b52ec5cd34bc89edd6425784113d86730d6a20c2
Gratss <snaelzzz@gmail.com>
2023-02-06 22:56:28 +0300

    #18

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -4,15 +4,18 @@ data(){
 return{
     visible: true,
     visible1: false,
+	hidden: true,
+    hidden1: false,
 }
 },
 }
 </script>
 Glebchikkk
 <template>
-<p v-if="visible">Supra</p>
-<p v-if="visible1">Toyota</p>
-
+<p v-if="visible">Toyota</p>
+<p v-if="!visible1">Supra</p>
+<p v-if="hidden">A80</p>
+<p v-if="!hidden1">1994</p>
 </template>
 
 <style >
