f4562b42f06c00b26c400deeed366474ead58e83
Gratss <snaelzzz@gmail.com>
2023-02-06 22:55:14 +0300

    #17

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -1,20 +1,18 @@
 <script>
 export default{
 data(){
+return{
+    visible: true,
+    visible1: false,
+}
 },
-methods: {
-show: function(){
-    alert(new Date().toLocaleDateString());
-}
-}
 }
 </script>
 Glebchikkk
 <template>
-<a class="inline-link-1" href="https://ru.wikipedia.org/wiki/Toyota_Supra" onclick="event.preventDefault()">Supra</a>
-<br>
-<br>
-<button @click.once="show">Supra</button>
+<p v-if="visible">Supra</p>
+<p v-if="visible1">Toyota</p>
+
 </template>
 
 <style >
