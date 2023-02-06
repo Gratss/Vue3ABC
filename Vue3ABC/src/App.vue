c754217d23491b7507d7e4ede4f00c32b6b223a4
Gratss <snaelzzz@gmail.com>
2023-02-06 22:46:47 +0300

    #9

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -1,15 +1,17 @@
 <script>
 export default{
 data(){
-return{
-    obj: {x: 1, y: 2, z: 3},
+},
+methods: {
+show: function(){
+    alert(new Date().toLocaleDateString());
 }
 }
 }
 </script>
 
 <template>
-<div>{{ obj.x + obj.y + obj.z }}</div>
+<button @click="show">data</button>
 </template>
 
 <style >
