140bf6205373466cc594f438b082acbf94abf9e1
Gratss <snaelzzz@gmail.com>
2023-02-06 22:49:22 +0300

    #11

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -1,18 +1,30 @@
 <script>
 export default{
 data(){
+return{
+    num1: 1,
+    num2: 2,
+    text: 'polina'
+}
 },
 methods: {
 show: function(){
-    alert(new Date().toLocaleDateString());
+    let text = this.cape(this.text)
+    alert(text);
+    // сумма чисел;
+},
+cape: function(str){
+    return str[0].toUpperCase() + str.slice(1);
 }
 }
 }
 </script>
 Glebchikkk
 <template>
-<button  v-on:mouseover="show">Наведение</button>
-<button  @click="show">Нажатие</button>
+<div>{{ text }}</div>
+<div>{{ num1 + num2 }}</div>
+<button v-on:mouseover="show">Наведение</button>
+<button @click="show">Нажатие</button>
 </template>
 
 <style >
