438f0a7e0e525468c78b6fa7cae888adf92bdb1e
Gratss <snaelzzz@gmail.com>
2023-02-13 15:09:06 +0300

    #36

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -2,48 +2,21 @@
 export default{
 data(){
 return{
-    SUPRA: ['Toyota','Supra','A800']
 }
-},
-methods: {
-add: function(){
-this.Supra.push('Moya');
-},
-del: function(){
-this.Supra.shift();
-},
-del1: function () {
-this.Supra.pop();
-},
-splice: function(){
-this.Supra.splice(2, 1);
-},
-sort: function(){
-this.Supra.sort();
-},
-reverse: function () {
-    this.Supra.reverse();
-},
 }
 }
 </script>
 Glebchikkk
 <template>
-<ul>
-<li v-for="elem in Supra">{{ elem }}</li>
-</ul>
-<button @click="add">1</button>
-<br>
-<button @click="del">2</button>
-<br>
-<button @click="del1">3</button>
-<br>
-<button @click="splice">4</button>
-<br>
-<button @click="sort">5</button>
-<br>
-<button @click="reverse">6</button>
+<p>Mazda</p>
+<p1>Toyota</p1>
 </template>
 
 <style >
+p {
+		color: rgb(0, 255, 238);
+	}
+p1 {
+		color: rgb(0, 13, 255);
+	}
 </style>
