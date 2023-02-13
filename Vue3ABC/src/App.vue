e3158cc176100d97adebef58b7028ad41a79df63
Gratss <snaelzzz@gmail.com>
2023-02-13 14:50:13 +0300

    #22

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -2,32 +2,15 @@
 export default{
 data(){
 return{
-    visible: true,
-    visible2: true,
-    visible3: true,
-}
-},
-methods: {
-Toyota: function(){
-    this.visible = !this.visible;
-},
-Supra: function () {
-    this.visible2 = !this.visible2;
-},
-A80: function () {
-    this.visible3 = !this.visible3;
+    SUPRA: false,
 }
 }
 }
 </script>
-Glebchikkk
+
 <template>
-<button @click="Toyota">Toyota</button>
-<p>{{visible ? 'Toyota' : '!'}}</p>
-<button @click="Supra">Supra</button>
-<p>{{visible2 ? 'Supra' : '!'}}</p>
-<button @click="A80">A80</button>
-<p>{{visible3 ? 'A80' : '!'}}</p>
+<p v-if="SUPRA">A80</p>
+<p v-else>Toyota</p>
 </template>
 
 <style >
