cc0f6ad8a06987689f3480f0c8fc962fc701d68e
Gratss <snaelzzz@gmail.com>
2023-02-13 14:58:50 +0300

    #29

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -2,14 +2,31 @@
 export default{
 data(){
 return{
-    SUPRA: ['x', 'y', 'z'],
+    SUPRA: {a:'100$', b:'200$', c:'300$'},
 }
 }
 }
 </script>
 Glebchikkk
 <template>
-<ul v-for="(elem, key) in supra"> {{ key }} {{ elem }}</ul>
+<ul>
+    <li v-for="elem in SUPRA">{{ elem }}</li>
+</ul>
+<br>
+<br>
+<ul>
+    <li v-for="(key, elem) in SUPRA"> {{ elem }} - {{ key }}</li>
+</ul>
+<br>
+<br>
+<ul>
+    <li v-for="(key, elem, index) in SUPRA"> {{ elem }} - {{ key }} - {{ index }}</li>
+</ul>
+<br>
+<br>
+<ul>
+    <li v-for="(key, elem, index) in SUPRA"> {{ elem }} - {{ key }} - {{ index+1 }}</li>
+</ul>
 </template>
 
 <style >
