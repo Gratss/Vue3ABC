6e58080b0a229c45e1d9774bc0040f60b52741c8
Gratss <snaelzzz@gmail.com>
2023-02-13 14:50:46 +0300

    #23

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -2,15 +2,19 @@
 export default{
 data(){
 return{
-    SUPRA: false,
+    day: 'Воскресенье',
 }
-}
-}
+}}
 </script>
-
+Glebchikkk
 <template>
-<p v-if="SUPRA">A80</p>
-<p v-else>Toyota</p>
+<p v-if="day === 'Понедельник'">Понедельник</p>
+<p v-if="day === 'Вторник'">Вторник</p>
+<p v-if="day === 'Среда'">Среда</p>
+<p v-if="day === 'Четверг'">Четверг</p>
+<p v-if="day === 'Пятница'">Пятница</p>
+<p v-if="day === 'Суббота'">Суббота</p>
+<p v-if="day === 'Воскресенье'">Воскресенье</p>
 </template>
 
 <style >
