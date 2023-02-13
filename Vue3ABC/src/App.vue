35a12049d79183bf28448cf775d6420a5e54931c
Gratss <snaelzzz@gmail.com>
2023-02-13 15:01:59 +0300

    #31

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -2,22 +2,46 @@
 export default{
 data(){
 return{
-    SUPRA: [1, 2, 3],
+    SUPRA: [
+    {href: '1.html',text: 'Gleb',},
+    {href: '2.html',text: 'Gleb',},
+    {href: '3.html',text: 'Gleb',},
+    ],
+    products: [
+    {
+        name: 'product1',
+        price: 100,
+        quantity: 5
+    },
+    {
+        name: 'product2',
+        price: 200,
+        quantity: 4
+    },
+    {
+        name: 'product3',
+        price: 300,
+        quantity: 3
+    },
+    ]
 }
 }
 }
 </script>
 Glebchikkk
 <template>
-<div v-for="elem in SUPRA">
-<p>{{ elem }}</p>
-<p class="divider"> </p>
-</div>
-<br>
-<ul v-for="elem in SUPRA">
-    <li>{{ elem }}</li>
-    <li class="divider"> </li>
+<ul class="styled">
+    <li v-for="link in SUPRA"><a SUPRA="{{ link.SUPRA }}">{{ link.text }}</a></li>
 </ul>
+<br>
+<br>
+<table>
+    <tr v-for="col in products">
+    <td>{{ col.name }}</td>
+    <td>{{ col.price }}</td>
+    <td>{{ col.quantity }}</td>
+    </tr>
+</table>
 </template>
 
 <style >
