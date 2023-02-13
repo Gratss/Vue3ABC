234f7917f051912eee10de21d8b8bdd853916ff0
Gratss <snaelzzz@gmail.com>
2023-02-13 15:07:36 +0300

    #35

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -2,27 +2,47 @@
 export default{
 data(){
 return{
-    SUPRA: [
-    {
-        id: 1,
-        name: 'Toyota',
-    },
-    {
-        id: 2,
-        name: 'Supra',
-    },
-    {
-        id: 3,
-        name: 'A80',
-    },
-    ]
+    SUPRA: ['Toyota','Supra','A800']
 }
+},
+methods: {
+add: function(){
+this.Supra.push('Moya');
+},
+del: function(){
+this.Supra.shift();
+},
+del1: function () {
+this.Supra.pop();
+},
+splice: function(){
+this.Supra.splice(2, 1);
+},
+sort: function(){
+this.Supra.sort();
+},
+reverse: function () {
+    this.Supra.reverse();
+},
 }
 }
 </script>
 Glebchikkk
 <template>
-<p class= "styled" v-for="prod in Supra" :key='Supra.id'>{{ prod.name }}</p>
+<ul>
+<li v-for="elem in Supra">{{ elem }}</li>
+</ul>
+<button @click="add">1</button>
+<br>
+<button @click="del">2</button>
+<br>
+<button @click="del1">3</button>
+<br>
+<button @click="splice">4</button>
+<br>
+<button @click="sort">5</button>
+<br>
+<button @click="reverse">6</button>
 </template>
 
 <style >
