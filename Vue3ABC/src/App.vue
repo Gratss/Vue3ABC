8c625f13b27b9b22616f69c4d8d6f27e3d3955cf
Gratss <snaelzzz@gmail.com>
2023-02-20 18:52:16 +0300

    52

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -1,34 +1,22 @@
+
 <script>
 export default{
 data(){
 return{
-    isDisabled: true,
-    isDisabled1: true,
-    checked: false,
+supra: ' ',
+supra: ' '
 }
-},
-methods: {
-Supra: function () {
-    this.isDisabled = false;
-},
-Supra: function () {
-    this.isDisabled1 = false;
-},
 }
 }
 </script>
+Glebchikkk
 
 <template>
-<input type="text" class="text-field__input"  v-bind:disabled="isDisabled">
+<input class="text-field__input" v-model="supra">
+<p>{{ supra }}</p>
 <br>
-<button @click="Supra">Supra</button>
-<br>
-<br>
-<br>
-<input type="text" class="text-field__input" v-bind:disabled="isDisabled1">
-<br>
-<input type="checkbox" v-model="checked" @click="Supra">
-<button @click="Supra">Supra</button> 
+<a href="#" class="inline-link-1" >Supra</a>
+<p ></p>
 </template>
 
 <style>
