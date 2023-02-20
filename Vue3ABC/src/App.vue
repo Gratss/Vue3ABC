70a9ae2b5bff97cbae36528bacd231246ac712a4
Gratss <snaelzzz@gmail.com>
2023-02-20 18:36:42 +0300

    41

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -2,47 +2,24 @@
 export default{
 data(){
 return{
-    cssClasses: 'Supra',
-    visible: true,
-    style: {
-    hidden: true,
-}
-}
-},
-methods: {
-setDone:function(){
-this.style.hidden = false;
-},
-setTwo: function(){
-this.style.hidden = true;
-},
-supra: function () {
-    this.visible = !this.visible;
+    isValid: true,
+    isDisabled: true,
 }
 }
 }
 </script>
-Glebchikkk
 
 <template>
-<button @click="supra">supra</button>
-<p class="supra" v-if="visible">supra</p>
-<p :class="style">Conclave</p>
-<button @click="setDone">показывает</button>
-<button @click="setTwo">прячет</button>
+<p :class="{SUPRA: isValid, disable: isDisabled}">supra</p>
 </template>
 
 <style >
-.Supra {
-    padding: 10px;
-    margin: 10px;
-    background: rgb(160, 204, 0);
-    color: rgb(255, 255, 255);
-    font-weight: bold;
-    text-decoration: none;
-    font-size: 20px;
-}
-p.hidden{
-display: none;
+p.supra{
+padding: 10px;
+margin: 10px;
+background: rgb(245, 226, 15);
+color: rgb(245, 22, 22);
+font-weight: bold;
+text-decoration: none;
 }
 </style>
