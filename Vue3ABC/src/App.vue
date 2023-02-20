169aae3a288f302334466fdbbb19bb0516c48e28
Gratss <snaelzzz@gmail.com>
2023-02-20 18:30:13 +0300

    38

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -3,6 +3,10 @@ export default{
 data(){
 return{
     cssClasses: 'Supra',
+    obj: {
+    done: true,
+    selected: false,
+}
 }
 }
 }
@@ -11,6 +15,7 @@ Glebchikkk
 
 <template>
 <p :class="cssClasses">Supra</p>
+<p :class="obj">A80</p>
 </template>
 
 <style >
@@ -19,6 +24,24 @@ Glebchikkk
     margin: 10px;
     background: rgb(160, 204, 0);
     color: rgb(255, 255, 255);
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+}
+.selected{
+    padding: 10px;
+    margin: 10px;
+    background: rgb(234, 129, 16);
+    color: white;
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+}
+.done {
+    padding: 10px;
+    margin: 10px;
+    background: rgb(20, 247, 213);
+    color: rgb(12, 51, 245);
     font-weight: bold;
     text-decoration: none;
     font-size: 20px;
