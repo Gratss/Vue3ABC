275823baeb85bf28dc92198d8dc43993d5e3ffeb
Gratss <snaelzzz@gmail.com>
2023-03-01 17:32:56 +0300

    61

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -3,10 +3,27 @@ import Supra from './components/User.vue'
 export default {
 data() {
     return {
-    name: 'Gleb',
-    salary: '95000',
-    age: '18',
-    }
+    Supra: [
+        {
+        id: 1,
+        name: 'Gleb',
+        salary: 95000,
+        age: 18
+        },
+        {
+        id: 2,
+        name: 'Edem',
+        salary: 90000,
+        age: 18
+        },
+        {
+        id: 3,
+        name: 'Edem',
+        salary: 90000,
+        age: 18
+        },
+    ],
+}
 },
 components: {
     Supra
@@ -16,7 +33,7 @@ components: {
 Glebchikkk
 
 <template>
-<Supra :name="name" :salary="salary" :age="age"/>
+<Supra v-for="supras in smeshariki" :name="supras.name" :salary="supras.salary" :age="supras.age" :key="supras.id" />
 </template>
 <style>
 </style>
