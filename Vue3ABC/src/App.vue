0d7ea071f8f7a02fab8897dd0659fb957f92e3b5
Gratss <snaelzzz@gmail.com>
2023-03-01 17:39:57 +0300

    66

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -1,27 +1,50 @@
 <script>
 import Supra from './components/User.vue'
 
+
 export default {
 data() {
     return {
-    
+    Supra: [
+        {
+        id: 1,
+        name: 'Toyota',
+        surn: 'Supra'
+        },
+        {
+        id: 2,
+        name: 'Supra',
+        surn: 'Supra'
+        },
+        {
+        id: 3,
+        name: 'A80',
+        surn: 'Supra'
+        },
+    ],
     }
 },
 components: {
     Supra
 },
 methods: {
-    SupraName(name) {
-    console.log(name);
-    },
+    remove(id) {
+    this.Supra = this.Supra.filter((Supras) => {
+        return Supras.id !== id;
+    })
+    }
 }
 }
-
 </script>
 Glebchikkk
 
 <template>
-<Supra @show="supraName"/>
+<Supra v-for   ="Supras in Supra"
+		:id     ="Supras.id"
+		:name   ="supras.name"
+		:surn   ="supras.surn"
+		@remove ="remove"
+		:key    ="supras.id"/>
 </template>
 <style>
 </style>
