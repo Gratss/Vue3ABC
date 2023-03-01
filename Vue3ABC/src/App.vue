08018ca6e1c6f35d5585d41fd498c9d06261622a
Gratss <snaelzzz@gmail.com>
2023-03-01 17:27:09 +0300

    57

--- Vue3ABC/src/App.vue
+++ Vue3ABC/src/App.vue
@@ -1,59 +1,16 @@
 <script>
-export default{
-data(){
-return{
-    students: [
-    {
-        id: 1,
-        name: 'Gleb',
-        surn: 'Selivanov',
-        salary: 100,
-				age: 18,
-    },
-    {
-        id: 2,
-        name: 'Edem',
-        surn: 'Seytumerov',
-        salary: 200,
-				age: 18,
-    },
-    {
-        id: 3,
-        name: 'Edem',
-        surn: 'Asanov',
-        salary: 300,
-				age: 18,
-    },
-    ],
-}
-},
-methods: {
-    edit(student) {
-    student.isEdit = true;
-},
-save(student) {
-    student.isEdit = false;
-},
+import Supra from './components/User.vue'
+export default {
+components: {
+    Supra
 }
 }
 </script>
 Glebchikkk
 
 <template>
-<table>
-<tr v-for="student in students" :key="student.id"> 
-<template v-if="!student.isEdit">
-{{ student.name }} {{ student.surn }} {{ student.salary }} {{ student.age }}<button class="button" @click="edit(student)">Изменить</button>
-</template>
-<template v-else>
-<input v-model="student.name">
-<input v-model="student.surn">
-<input v-model="student.salary">
-<input v-model="student.age"><button class="button" @click="save(student)">Сохранить</button>
-</template>
-</tr>
-</table>
+<Supra />
 </template>
 
-<style scoped>
+<style>
 </style>
